9b584d3bcfb7bf784919f83b1487d0cc19abe4ae
ismatullayevs <ismatullayevpro@gmail.com>
2023-09-25 10:50:26 +0500

    Use environment variables

--- send_mail.py
+++ send_mail.py
@@ -1,30 +1,27 @@
 import smtplib
+from dotenv import load_dotenv
 from email.message import EmailMessage
-import ssl
+import ssl, os
 
-email_sender = 'alikatraveluz@gmail.com'
-email_password = 'nald jaqb yykl nbww'
-email_receiver = 'support@alika.uz'
-port = 465
+load_dotenv()
 
-subject = 'Test Subject'
-body = 'Test message1'
-
+EMAIL_HOST = os.getenv('EMAIL_HOST', '')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 0))
+EMAIL_RECEIVER = 'support@alika.uz'
+print(EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_PORT, EMAIL_RECEIVER)
 context = ssl.create_default_context()
 
 def send_mail(name, email, body):
     em = EmailMessage()
     em['From'] = f"{name} {email}"
-    em['To'] = email_receiver
-    em['subject'] = subject
+    em['To'] = EMAIL_RECEIVER
     body = f"""Name: {name}
 Email: {email}
 Message: {body}"""
     em.set_content(body)
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
-        server.login(email_sender, email_password)
-        server.sendmail(email_sender, email_receiver, em.as_string())
-
-
-send_mail('Javohir', 'example@gmail.com', 'Hey there')
+    with smtplib.SMTP_SSL(EMAIL_HOST, EMAIL_PORT, context=context) as server:
+        server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+        server.sendmail(EMAIL_HOST_USER, EMAIL_RECEIVER, em.as_string())
